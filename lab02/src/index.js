9ec8ef18c7576760245a5f37cf52d5d151e6f9e2
Wojciech Gola <wojciech.maciej.gola@wp.pl>
2022-10-12 15:33:59 +0200

    Lab02 - commit3

--- lab02/src/index.js
+++ lab02/src/index.js
@@ -2,4 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<h1>Hello, world!</h1>);
+
+// const name = "Jan Kowalski"
+
+// const url = "https://www.reactjs.org"
+
+// const element = (
+//     <div>
+//         <h1>Hello, {name}! <h2><a href={url}>Click here!</a></h2></h1>
+//     </div>
+// );
+
+// const element = React.createElement('h1', {className: 'greeting'}, 'Hello, world!');
+
+// root.render(element);
+
+const tick = () => {
+    const element = (
+        <div>
+            <h1>Hello, world!</h1>
+            <h2>It is {new Date().toLocaleTimeString()}</h2>
+        </div>
+    );
+    root.render(element);
+}
+
+setInterval(tick, 1000)
