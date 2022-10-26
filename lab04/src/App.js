7783f2373459d69c4686dc33ea5a7de46a6a6bf8
Wojciech Gola <wojciech.maciej.gola@wp.pl>
2022-10-26 15:41:32 +0200

    Lab04 - commit02

--- lab04/src/App.js
+++ lab04/src/App.js
@@ -1,6 +1,27 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
 function App() {
+  
+  const [ products, setProducts ] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://fakestoreapi.com/products")
+      .then(res => setProducts(res.data))
+  }, [])
+
   return (
-    <div>test</div>
+    <ul>
+      {products.map(product => (
+        <li key={product.id}>
+          <p>{product.id}</p>
+          <p>{product.title}</p>
+          <p>{product.price}</p>
+          <p>{product.category}</p>
+        </li>
+      ))}
+    </ul>
   );
 }
 
