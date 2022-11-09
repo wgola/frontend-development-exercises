239711156edea5c51c605684700b35c1c15a22a3
Wojciech Gola <wojciech.maciej.gola@wp.pl>
2022-11-09 15:43:26 +0100

    Lab05 - commit01

---
+++ lab05/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Basic from './Basic';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+   <Basic />
+);
